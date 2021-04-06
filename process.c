e2d2adf4d9f271cb9fe7ecfd6b82ca44e16d6b42
John O'Donnell <johnrodonnell77@gmail.com>
2021-04-05 21:15:23 -0400

    Everything before process_select should be good.

--- process.c
+++ process.c
@@ -16,10 +16,15 @@ struct process_state {
 	struct process_state* nextProcess;
 };
 
-process_t* listStart = NULL;
+process_t* process_queue = NULL;
+process_t* current_process = NULL;
 
 
 int process_create (void (*f)(void), int n) {
+	// Disable Global interrupts
+	PIT->CHANNEL[0].TCTRL = 1;
+
+
 	unsigned int * procPointer =  process_stack_init(*f, n);
 
 	if(procPointer == NULL) {
@@ -27,24 +32,28 @@ int process_create (void (*f)(void), int n) {
 	} else {
 		//Initializes the process to be added onto the linked list
 		process_t* elementPt;
-		elementPt = malloc(sizeof(process_t));
+		elementPt = malloc(n*sizeof(process_t));
 		elementPt->sp = procPointer;
 		elementPt->nextProcess = NULL;
 		elementPt->size = n;
 
 		//Make this the only element of the list if the list is empty
-		if(listStart == NULL) {
-			listStart = elementPt;
+		if(process_queue == NULL) {
+			process_queue = elementPt;
 
 		} else {
 			//Create a temporary process state, and keep updating it until it is the last element
 			process_t* tempProcessPt;
-			tempProcessPt = listStart;
+			tempProcessPt = process_queue;
 			while (tempProcessPt->nextProcess != NULL) {
 				tempProcessPt = tempProcessPt->nextProcess;
 			}
 			tempProcessPt->nextProcess = elementPt;
 		}
+
+		// Re-enable global interrupts
+		PIT->CHANNEL[0].TCTRL = 3;
+
 		return 0;
 	}
 
@@ -56,10 +65,20 @@ void process_start(void) {
 	PIT->CHANNEL[0].LDVAL = 0xFFF00; // Set load value of zeroth PIT
 	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK; // Enable timer for interrupts
 	NVIC_EnableIRQ(PIT_IRQn);
-	if(listStart == NULL) { return; }
+	if(process_queue == NULL) { return; }
 	process_begin();
 }
 
-unsigned int * process_select(unsigned int * cursp){
-	return 0;
+unsigned int * process_select(unsigned int * cursp) {
+	if ((cursp == NULL) || (process_queue == NULL)) {
+		return NULL;
+	}
+
+
+	if (current_process->nextProcess == NULL) {
+		current_process = process_queue;
+	} else {
+		current_process = current_process->nextProcess;
+	}
+	return current_process->sp;
 }
